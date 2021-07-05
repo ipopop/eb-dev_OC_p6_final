71c179891dcd4fe1093a5f33033e56ddce14fa56
ipopop <popop@ntymail.com>
2021-07-05 09:59:52 +0200

    4. install 'Express' & create 'app.js'

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 'use strict'
 
+require('dotenv').config()
 const http = require('http')
+const app = require('./app')
 
 const normalizePort = val => {
   const port = parseInt(val, 10)
+
   if (isNaN(port)) { return val }
+  
   if (port >= 0) { return port }
 }
 
 const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
 
 const errorHandler = error => {
   if (error.syscall !== 'listen') { throw error }
+
   const address = server.address()
   const bind = (typeof address === 'string') ? ('pipe ' + address) : ('port: ' + port)
   
@@ -20,22 +26,26 @@ const errorHandler = error => {
       console.error(bind + ' requires elevated privileges.')
       process.exit(1)
       break
+  
     case 'EADDRINUSE':
       console.error(bind + ' is already in use.')
       process.exit(1)
       break
-    default:
-    throw error
+  
+      default:
+      throw error
   }
 }
 
-const server = http.createServer()
+const server = http.createServer(app)
 
 server.on('error', errorHandler)
 
 server.on('listening', () => {
   const address = server.address()
+
   const bind = (typeof address === 'string') ? ('pipe ' + address) :  port
+
   console.log('Listening on http://localhost:' + bind)
 })
 
